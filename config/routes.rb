b22f033eb4cd4a90ca8a890fa054e4816816b34f
Hank Liu <hanke.liu@gmail.com>
2020-06-25 15:31:21 -0400

    config home and app route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/home'
-  get 'welcome/app', 'welcome#app'
+  get 'welcome/app', to: 'welcome#app', as: 'app'
   root 'welcome#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
